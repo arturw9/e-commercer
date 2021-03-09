fe9889fc989af32262b8857260dc1cd29f0e3faa
Artur Fidelis <65771411+arturfidelis-avoubiclabs@users.noreply.github.com>
2021-03-08 21:50:54 -0300

    inserir

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Header from './Header'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from './Login'
+import Home from './Home'
+import Checkout from './Checkout'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">           
+        <Switch>
+          <Route path="/checkout">
+            <Header/>
+            <Checkout/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/">
+              <Header/>
+              <Home/>
+          </Route>
+        </Switch>
+          
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
